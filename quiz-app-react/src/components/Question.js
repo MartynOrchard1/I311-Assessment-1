d0b7263de6e0ecc4cbcf14136559022314453b08
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-28 19:50:48 +1200

    Update Question.js

--- quiz-app-react/src/components/Question.js
+++ quiz-app-react/src/components/Question.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function Question({ question, onAnswer, userAnswer }) {
+  // Handle selecting a multiple choice option
   const handleOptionClick = (option) => {
     onAnswer(option);
   };
 
+  // Handle selecting true/false
   const handleTrueFalseClick = (value) => {
     onAnswer(value);
   };
@@ -13,6 +15,7 @@ function Question({ question, onAnswer, userAnswer }) {
     <div className="card p-4">
       <h4>{question.question}</h4>
 
+      {/* Render multiple-choice options */}
       {question.type === "multiple-choice" ? (
         <div className="mt-3">
           {question.options.map((option, index) => (
@@ -28,6 +31,7 @@ function Question({ question, onAnswer, userAnswer }) {
           ))}
         </div>
       ) : (
+        // Render true/false buttons
         <div className="mt-3">
           <button
             className={`btn btn-outline-success m-2 ${
