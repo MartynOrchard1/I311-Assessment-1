a73904f52f53547904426cc2b8892e2c6a46e797
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-04-28 19:44:00 +1200

    Update App.js

--- quiz-app-react/src/App.js
+++ quiz-app-react/src/App.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap for styling
 function App() {
   return (
     <div className="App">
+      {/* Renders the main quiz component */}
       <Quiz />
     </div>
   );
